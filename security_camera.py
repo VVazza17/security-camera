395044e4f65371367497d40a053853328df1e923
Kyle Deng <kyle.deng01@gmail.com>
2024-12-30 01:09:11 -0500

    AWS S3 implementation

--- security_camera.py
+++ security_camera.py
@@ -1,16 +1,18 @@
 import subprocess
 import time
+import boto3
 from datetime import datetime
 
 def record_video(duration):
 	# Current time to create unique filename
 	current_time = datetime.now().strftime("%Y%m%d%H%M%S")
 	filename = f'/home/wazza/Videos/video_{current_time}.h264'
+	filepath = f'/home/pi/Videos/{filename}'
 	
 	# Start recording using libcamera-vid
 	command = [
 	'libcamera-vid',
-	'-o', '/home/wazza/Videos/video.h264', # Output file
+	'-o', filepath, # Output file
 	'-t', str(duration * 1000), # Recording time in ms
 	'--inline', # Inline headers for h264
 	'--codec', 'h264', # Video codec to use 
@@ -19,10 +21,23 @@ def record_video(duration):
 	]
 	
 	print("Video recording started...")
-	process = subprocess.Popen(command) # Starts recording
-	time.sleep(duration) # Waits for duration to pass
-	process.terminate() # Stops recording
+	subprocess.Popen(command).wait()
 	print("Video recording stopped...")
 	
+	upload_to_s3(filepath, filename)
+	
+def upload_to_s3(file_path, file_name):
+	# Create an S3 client
+	s3 = boto3.client('s3')
+	bucket_name = 'wazzacam-bucket'
+	
+	print('Uploading to S3...')
+	
+	try:
+		s3.upload_file(file_path, bucket_name, file_name)
+		print("Upload complete.")
+	except Exception as e:
+		print(f"Failed to upload to S3: {e}")
+
 if __name__ == "__main__":
 	record_video(60)
